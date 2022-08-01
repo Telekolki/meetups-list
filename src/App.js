1f6875d3caa7e93e6cbe46c3330f2b6d62bdb9f6
Gribs <telekolki@gmail.com>
2022-08-01 16:20:43 +0300

    removed todo's content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import Todo from './components/Todo';
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <div>
-        <Todo text="Refresh React" />
-        <Todo text="Learn Next.JS" />
-        <Todo text="Learn D3" />
-        <Todo text="Finish the portfolio project" />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
