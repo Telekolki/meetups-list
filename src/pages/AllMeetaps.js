4783ae54dfe85d1d3bf7ff704e2dc3fd0fecbc62
Gribs <telekolki@gmail.com>
2022-08-08 18:01:50 +0300

    fetching data from firebase

--- src/pages/AllMeetaps.js
+++ src/pages/AllMeetaps.js
@@ -1,31 +1,68 @@
-import MeetupList from "../components/meetups/MeetupList";
+import { useEffect, useState } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
 
 const AllMeetaupsPage = () => {
-  const DUMMY_DATA = [
-    {
-      id: "m1",
-      title: "This is a first meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Meetupstreet 5, 12345 Meetup City",
-      description:
-        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    },
-    {
-      id: "m2",
-      title: "This is a second meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Meetupstreet 5, 12345 Meetup City",
-      description:
-        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    },
-  ];
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://react-meetups-2c8cb-default-rtdb.europe-west1.firebasedatabase.app/meetups.json'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        setIsLoading(false);
+        if (!!result) {
+          const newData = [];
+
+          // not good
+          // Object.entries(result).map((el) => {
+          //   newData.push({
+          //     id: el[0],
+          //     ...el[1],
+          //   });
+          // });
+
+          // better
+          for (const key in result) {
+            newData.push({
+              id: key,
+              ...result[key],
+            });
+          }
+          setData([...newData]);
+        }
+      });
+  }, []);
+
+  // const DUMMY_DATA = [
+  //   {
+  //     id: 'm1',
+  //     title: 'This is a first meetup',
+  //     image:
+  //       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+  //     address: 'Meetupstreet 5, 12345 Meetup City',
+  //     description:
+  //       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  //   },
+  //   {
+  //     id: 'm2',
+  //     title: 'This is a second meetup',
+  //     image:
+  //       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+  //     address: 'Meetupstreet 5, 12345 Meetup City',
+  //     description:
+  //       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  //   },
+  // ];
 
   return (
     <div>
       <ul>
-        <MeetupList meetups={DUMMY_DATA} />
+        {!isLoading ? <MeetupList meetups={data} /> : <div>Loading...</div>}
       </ul>
     </div>
   );
